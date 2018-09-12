22174e109e33664e6ec11d18507792e0f8c8cc18
Jeremy Zhang <41759551+jeremyzhang1@users.noreply.github.com>
2018-09-12 17:26:57 -0500

    Update G.java

--- Fricso2016/G.java
+++ Fricso2016/G.java
@@ -8,13 +8,26 @@ public class G {
 		Scanner in = new Scanner(new File("G.in"));
 		while(in.hasNextInt()) {
 			int n = in.nextInt();
+			if(n == 0) {
+				break;
+			}
 			in.nextLine();
 			int p = 0;
+			int runsum = 0;
 			for(int i = 0; i < n; i++) {
 				String query = in.nextLine();
-				if(query.equals("touchdown")) p+=7;
-				if(query.equals("field goal")) p += 3;
-				if(query.equals("safety")) p +=2;
+				if(query.equals("touchdown")) {
+					runsum += 7;
+					p += runsum;
+				}
+				if(query.equals("field goal")) {
+					runsum += 3;
+					p += runsum;
+				}
+				if(query.equals("safety")) {
+					runsum += 2;
+					p += runsum;
+				}
 			}
 			System.out.println("Ensign Redshirt does " + p + " pushups!");
 		}
