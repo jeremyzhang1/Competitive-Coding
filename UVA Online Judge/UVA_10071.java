6ca09f089c557f6b9f34122b6e6a3c68c6e0cda3
JB Ladera <32505118+MiniDomo@users.noreply.github.com>
2018-10-12 17:59:47 -0500

    Update UVA_10071.java
    further simplified the code within the loop

--- UVA Online Judge/UVA_10071.java
+++ UVA Online Judge/UVA_10071.java
@@ -5,9 +5,7 @@ public class UVA_10071 {
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		while(in.hasNextInt()){
-			int a = in.nextInt();
-			int b = in.nextInt();
-			System.out.println(a*b*2);
+			System.out.println(in.nextInt() * in.nextInt() * 2);
 		}
 	}
 }
